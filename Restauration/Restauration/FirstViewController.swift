47a67666d53269ed169f53a76d0f39fcbf3511b6
marylinkana <43268881+marylinkana@users.noreply.github.com>
2020-01-08 09:07:26 +0100

    Initialisation du projet restauration

---
+++ Restauration/Restauration/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Restauration
+//
+//  Created by Administrateur on 08/01/2020.
+//  Copyright © 2020 Administrateur. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Restauration/Restauration/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Restauration
+//
+//  Created by Administrateur on 08/01/2020.
+//  Copyright © 2020 Administrateur. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
