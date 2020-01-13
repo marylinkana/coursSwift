18abe48adb9149424020480bdf91f60e5471ae77
marylinkana <43268881+marylinkana@users.noreply.github.com>
2020-01-13 10:18:57 +0100

    nouvelle version du projet restauration

---
+++ Projet Resto 2020 v2/EceResto2020v2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EceResto2020v2
+//
+//  Created by Gian on 08/01/2020.
+//  Copyright © 2020 Gian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Projet Resto 2020 v2/EceResto2020v2/SecondViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SecondViewController.swift
+//  EceResto2020v2
+//
+//  Created by Gian on 08/01/2020.
+//  Copyright © 2020 Gian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var arrayCategories = [[String:String]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let c1 = ["idc":"1","nomc":"Entrées"]
+        let c2 = ["idc":"2","nomc":"Plats"]
+        let c3 = ["idc":"3","nomc":"Soupes"]
+        let c4 = ["idc":"4","nomc":"Desserts"]
+        let c5 = ["idc":"5","nomc":"Boissons"]
+
+    arrayCategories = [c1, c2, c3, c4, c5]
+        
+    }
+    
+    func nombreDeSections(in tableView: UITableView) -> Int{
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellule = tableView.dequeueReusableCell(withIdentifier: "cellCategorie", for: indexPath) as! CategorieTableViewCell
+        let nomCategorie = arrayCategories[indexPath.row]["nomc"]
+        
+        // cellule.textLabel?.text = nomCategorie
+        cellule.nomCatCell.text = nomCategorie
+        
+        return cellule
+    }
+}
