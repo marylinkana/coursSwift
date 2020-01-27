2322937b04d6bf4e185f189bc1e31918e64b4628
marylinkana <43268881+marylinkana@users.noreply.github.com>
2020-01-27 11:51:55 +0100

    popup

--- Projet Resto 2020 v2.2 au 21 janvier 2020 avec BDD et ajout table Categories/EceResto2020v2/DescriptionPlatTableViewController.swift
+++ Projet Resto 2020 v2.2 au 21 janvier 2020 avec BDD et ajout table Categories/EceResto2020v2/DescriptionPlatTableViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class DescriptionPlatTableViewController: UITableViewController {
-
+    // ajoug de produit
+    var quantitePlat:Int!
+    @IBOutlet var viewContainer: UIView!
+    @IBAction func diminuerQuantite(_ sender: Any) {
+    }
+    @IBAction func augmenterQuantite(_ sender: Any) {
+    }
+    @IBOutlet var quantitePlatsLabel: UILabel!
+    
+    var paramettres2 = [String:String]()
+    
+    @IBOutlet var imageP: UIImageView!
+    @IBOutlet var nomP: UILabel!
+    @IBOutlet var prixP: UILabel!
+    @IBOutlet var descP: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        quantitePlat = 1
+        quantitePlatsLabel.text = "\(quantitePlat)"
+        
+        
+        //valeur de départ
+        nomP.text = paramettres2["nom_plat"]
+        prixP.text = paramettres2["prix_plat"]
+        descP.text = paramettres2["desc_plat"]
+        //imageP.image = UIImage(named: archiveP)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,6 +43,15 @@ class DescriptionPlatTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    @IBAction func ajoutFavoris(_ sender: Any) {
+    }
+    
+    @IBAction func commander(_ sender: Any) {
+        viewContainer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        viewContainer.center = view.center
+        self.view.addSubview(viewContainer)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
