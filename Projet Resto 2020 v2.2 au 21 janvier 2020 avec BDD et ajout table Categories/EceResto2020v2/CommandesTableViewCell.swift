60f67562d89aca5eaae35008c59aff946e0681cc
marylinkana <43268881+marylinkana@users.noreply.github.com>
2020-02-03 18:13:19 +0100

    commande tablea view cell

---
+++ Projet Resto 2020 v2.2 au 21 janvier 2020 avec BDD et ajout table Categories/EceResto2020v2/CommandesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommandeTableViewCell.swift
+//  EceResto2020v2
+//
+//  Created by Administrateur on 03/02/2020.
+//  Copyright © 2020 Gian. All rights reserved.
+//
+
+import UIKit
+
+class CommandesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
